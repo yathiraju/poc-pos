3efe041764a86df4710abbd243f9eb694db0dd84
yathi <kolli.yathi@gmail.com>
2020-04-13 17:26:58 -0400

    modified configuration and added database details

--- src/test/java/com/pos/PocPosApplicationTests.java
+++ src/test/java/com/pos/PocPosApplicationTests.java
@@ -1,13 +1,10 @@
 package com.pos;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PocPosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
