f8532adc0015dc56857514f94abf87953b60db98
yathi <kolli.yathi@gmail.com>
2020-04-13 14:18:05 -0400

    first commit

---
+++ src/main/java/com/pos/PocPosApplication.java
@@ -0,0 +1,13 @@
+package com.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocPosApplication.class, args);
+	}
+
+}
